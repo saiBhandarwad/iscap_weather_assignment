430321cc88533c0350180ad62e9ab772bad313c3
saiBhandarwad <s333bhandarwad@gmail.com>
2023-11-07 10:29:29 +0530

    upcoming days created

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const searchBtn = document.getElementById('searchBtn')
 cityName.value = 'pune'
 handleSearch('pune')
 
-async function handleSearch(city){
+async function handleSearch(city) {
     console.log(city);
 
     const url = `https://foreca-weather.p.rapidapi.com/location/search/${city}?lang=en&country=in`;
@@ -24,6 +24,7 @@ async function handleSearch(city){
         cityId = result.locations[0].id
         getCurrentWeather(cityId)
         getWeatherByThreeHourly(cityId)
+        getUpcomingDaysWeather(cityId)
     } catch (error) {
         console.error(error);
     }
@@ -43,35 +44,36 @@ const getCurrentWeather = async (id) => {
         const result = await response.json();
 
         document.getElementById('temperature').innerHTML = result.current.feelsLikeTemp + '&deg;'
+        document.getElementById('chances_of_rain').innerHTML = result.current.symbolPhrase
 
         /* changing icon according to response */
-        if(result.current.symbolPhrase === "partly cloudy"){
+        if (result.current.symbolPhrase === "partly cloudy") {
             document.getElementById('main_weather_icon').innerHTML = '<i class="fa-solid fa-cloud-sun "></i>'
         }
-        else if(result.current.symbolPhrase === "cloudy"){
+        else if (result.current.symbolPhrase === "cloudy") {
             document.getElementById('main_weather_icon').innerHTML = '<i class="fa-solid fa-cloud"></i>'
         }
-        else if(result.current.symbolPhrase === "clear"){
+        else if (result.current.symbolPhrase === "clear") {
             document.getElementById('main_weather_icon').innerHTML = '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>'
         }
-        else{
+        else {
             document.getElementById('main_weather_icon').innerHTML = '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>'
         }
 
         /* changing air conditions data */
         document.getElementById('real_feel_num_data').innerHTML = result.current.feelsLikeTemp
-        document.getElementById('humidity').innerHTML = result.current.relHumidity+'%'
+        document.getElementById('humidity').innerHTML = result.current.relHumidity + '%'
         console.log(result);
         document.getElementById('windDirection').innerHTML = result.current.windDirString
         console.log(result);
-        document.getElementById('windSpeed').innerHTML = result.current.windSpeed+'km/hr'
+        document.getElementById('windSpeed').innerHTML = result.current.windSpeed + 'km/hr'
         document.getElementById('weatherCondition').innerHTML = result.current.symbolPhrase
-        
+
     } catch (error) {
         console.error(error);
     }
 }
-const getWeatherByThreeHourly = async(id) => {
+const getWeatherByThreeHourly = async (id) => {
     const url = `https://foreca-weather.p.rapidapi.com/forecast/3hourly/${id}?alt=0&tempunit=C&windunit=MS&tz=Europe%2FLondon&periods=8&dataset=full&history=0`;
     const options = {
         method: 'GET',
@@ -89,18 +91,47 @@ const getWeatherByThreeHourly = async(id) => {
         document.getElementById('city').innerHTML = cityName.value
 
         /* changing innerhtml with data */
-        result.forecast.map((elem,i,arr)=>{
+        result.forecast.map((elem, i, arr) => {
             document.getElementById('hourly_weather').innerHTML += `<div class="forecast_data_hourly">
-            <div class="forecast_time">${elem?.time?.slice(-6,-1)}</div>
+            <div class="forecast_time">${elem?.time?.slice(-6, -1)}</div>
             <div class="forecast_icon"> ${elem.symbolPhrase === 'clear' ? '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>' : elem.symbolPhrase === 'cloudy' ? '<i class="fa-solid fa-cloud"></i>' : elem.symbolPhrase === 'partly cloudy' ? '<i class="fa-solid fa-cloud-sun "></i>' : '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>'}</div>
             <div class="forecast_temp">${elem?.feelsLikeTemp}&deg;</div>
-        </div>${i == arr.length-1 ? '' : '<div class="verticle_line"></div>'}
+        </div>${i == arr.length - 1 ? '' : '<div class="verticle_line"></div>'}
         `
         })
-        
+
     } catch (error) {
         console.error(error);
     }
 }
+async function getUpcomingDaysWeather(id) {
+    const url = `https://foreca-weather.p.rapidapi.com/forecast/daily/${id}?alt=0&tempunit=C&windunit=MS&periods=8&dataset=full`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '79c6b4dd17msh99173bc8048d9b6p1aba9bjsn473741998611',
+            'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+        }
+    };
 
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        document.getElementById('upcomingDays').innerHTML=""
+        result.forecast.map((elem,i,arr)=>{document.getElementById('upcomingDays').innerHTML+=` <div class="wrapper">
+        <div class="flex_wrapper">
+            <div class="day">${new Date(elem.date).toString().slice(0,3)}</div>
+            <div class="condition"> <span> ${elem.symbolPhrase === 'clear' ? '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>' : elem.symbolPhrase === 'cloudy' ? '<i class="fa-solid fa-cloud"></i>' : elem.symbolPhrase === 'partly cloudy' ? '<i class="fa-solid fa-cloud-sun "></i>' : '<i class="fa-solid fa-sun" style="color: #f07b3d;"></i>'}</span>
+                <span>${elem.symbolPhrase}</span></div>
+            <div class="temp">${elem.maxTemp+'/'+elem.minTemp}</div>
+        </div>
+        ${i == arr.length - 1 ? '' : '<div class="horizontal_divider"></div>'}
+        
+    </div>`})
+        
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
 searchBtn.addEventListener('click', () => handleSearch(cityName.value))
